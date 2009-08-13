ce9b06cb0efdfab36f447c37082b80eb31f3812c
andreassolberg <andreassolberg@04838e40-87ce-11de-9934-f1fc26e2a4cc>
2009-08-13 08:52:52 +0000

    add confiugration file

---
+++ modules/virtualorg/config-template/module_virtualorg.php
@@ -0,0 +1,11 @@
+<?php
+/* 
+ * The configuration of simpleSAMLphp statistics package
+ */
+
+$config = array (
+	'admins' => array('andreas@rnd.feide.no'),
+	'auth' => 'saml2',
+	'useridattr' => 'eduPersonPrincipalName',
+);
+
