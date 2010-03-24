ad84521306ef3df5662edff8a6c4cb2c20eebfb4
andreassolberg <andreassolberg@04838e40-87ce-11de-9934-f1fc26e2a4cc>
2010-03-24 15:45:54 +0000

    preparing for translation

--- trunk/modules/selfregister/templates/reviewuser.tpl.php
+++ trunk/modules/selfregister/templates/reviewuser.tpl.php
@@ -13,16 +13,19 @@ $this->includeAtTemplateBase('includes/header.php'); ?>
 
 <h1><?php echo $this->t('review_head'); ?></h1>
 <p>
-<?php echo $this->t('review_intro', array('%UID%' => $this->data['uid']) ); ?>
-<ul>
-<li><a href="reviewUser.php?logout=true">Logout</a></li>
-<li><a href="changePassword.php"><?php echo $this->t('link_changepw'); ?></a></li>
-</ul>
+	<?php echo $this->t('review_intro', array('%UID%' => '<b>' . $this->data['uid'] . '</b>') ); ?>
 </p>
+
 
 <?php if(isset($this->data['userMessage'])){ ?>
 	  <div class="mesg"><?php echo $this->data['userMessage']; ?></div>
 <?php }?>
 <?php print $this->data['formHtml']; ?>
+
+<ul>
+	<li><a href="reviewUser.php?logout=true"><?php echo $this->t('{status:logout}'); ?></a></li>
+	<li><a href="changePassword.php"><?php echo $this->t('link_changepw'); ?></a></li>
+</ul>
+
 
 <?php $this->includeAtTemplateBase('includes/footer.php'); ?>
